94ecfac3ca3beaeac6ec7c59e85bf46172551bb2
bariq <peters.norman91@gmail.com>
2024-08-11 17:18:33 +0200

    language toggle adjusted

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,20 +1,30 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
-import './Navbar.css';
+import React, { useState } from 'react';
+import { AppBar, Toolbar, Typography, Button, Switch, Box } from '@mui/material';
 import { useTranslation } from 'react-i18next';
+import Flag from 'react-world-flags';
+import './Navbar.css';
 
 const LanguageSwitcher: React.FC = () => {
     const { i18n } = useTranslation();
+    const [language, setLanguage] = useState(i18n.language === 'de');
 
-    const changeLanguage = (lng: string) => {
-        i18n.changeLanguage(lng);
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const isEnglish = event.target.checked;
+        const newLanguage = isEnglish ? 'en' : 'de';
+        i18n.changeLanguage(newLanguage);
+        setLanguage(isEnglish);
     };
 
     return (
-        <div>
-            <button onClick={() => changeLanguage('de')}>Deutsch</button>
-            <button onClick={() => changeLanguage('en')}>English</button>
-        </div>
+        <Box display="flex" alignItems="center">
+            <Flag code="DE" style={{ width: 24, height: 24 }} />
+            <Switch
+                checked={language}
+                onChange={handleChange}
+                color="default"
+            />
+            <Flag code="GB" style={{ width: 24, height: 24 }} />
+        </Box>
     );
 };
 
@@ -30,11 +40,10 @@ const Navbar: React.FC = () => {
         <AppBar position="fixed">
             <Toolbar>
                 <Typography variant="h6" style={{ flexGrow: 1 }}>
-                    {/* Hier kannst du einen Titel oder Logo platzieren */}
                 </Typography>
                 <Button color="inherit" onClick={() => scrollToSection('home')}>Home</Button>
                 <Button color="inherit" onClick={() => scrollToSection('about')}>About</Button>
-                <Button color="inherit" onClick={() => scrollToSection('projects')}>Projects</Button>
+                <Button color="inherit" onClick={() => scrollToSection('projects')} sx={{marginRight: 3}}>Projects</Button>
                 <LanguageSwitcher />
             </Toolbar>
         </AppBar>
