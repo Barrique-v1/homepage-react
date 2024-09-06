1fe10272a0088d94b0658d822741a7b2d809bd03
Barrique-v1 <peters.norman91@gmail.com>
2024-09-06 22:02:47 +0200

    adjust Links

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Grid, Typography, Box } from '@mui/material';
+import {Box, Grid, Typography} from '@mui/material';
 import ProjectCard from './ProjectCard';
 import BucksBuddyLogo from '../../assets/images/BucksBuddy_Logo.png';
-import { SiTypescript, SiVuedotjs, SiSpringboot, SiPostgresql, SiNuxtdotjs, SiBootstrap } from 'react-icons/si';
-import { FaGolang } from "react-icons/fa6";
-import { ReactComponent as JavaIcon } from '../../assets/icons/java.svg';
-import { useTranslation } from "react-i18next";
+import {SiBootstrap, SiNuxtdotjs, SiPostgresql, SiSpringboot, SiTypescript, SiVuedotjs} from 'react-icons/si';
+import {FaGolang} from "react-icons/fa6";
+import {ReactComponent as JavaIcon} from '../../assets/icons/java.svg';
+import {useTranslation} from "react-i18next";
 
 const Projects: React.FC = () => {
-    const { t } = useTranslation();
+    const {t} = useTranslation();
     return (
         <Box
             sx={{
@@ -30,20 +30,20 @@ const Projects: React.FC = () => {
             <Grid container spacing={3}>
                 <Grid item xs={12} md={6}>
                     <ProjectCard
-                        title="Travel Fund"
+                        title="Bucks Buddy"
                         image={BucksBuddyLogo}
                         description={t('projects.trafelfund')}
                         button_text={t('projects.button')}
                         link="https://bucks-buddy.onrender.com"
                         frontend_technologies={[
-                            { name: 'TypeScript', icon: <SiTypescript fontSize="48px" style={{ color: '#3178C6' }} /> },
-                            { name: 'Vue.js', icon: <SiVuedotjs fontSize="48px" style={{ color: '#42b883' }} /> },
-                            { name: 'Bootstrap', icon: <SiBootstrap fontSize="48px" style={{ color: '#7952B3' }} /> },
+                            {name: 'TypeScript', icon: <SiTypescript fontSize="48px" style={{color: '#3178C6'}}/>},
+                            {name: 'Vue.js', icon: <SiVuedotjs fontSize="48px" style={{color: '#42b883'}}/>},
+                            {name: 'Bootstrap', icon: <SiBootstrap fontSize="48px" style={{color: '#7952B3'}}/>},
                         ]}
                         backend_technologies={[
-                            { name: 'Java', icon: <JavaIcon width={48} height={48} /> },
-                            { name: 'Spring Boot', icon: <SiSpringboot fontSize="48px" style={{ color: '#6DB33F' }} /> },
-                            { name: 'PostgreSQL', icon: <SiPostgresql fontSize="48px" style={{ color: '#336791' }} /> },
+                            {name: 'Java', icon: <JavaIcon width={48} height={48}/>},
+                            {name: 'Spring Boot', icon: <SiSpringboot fontSize="48px" style={{color: '#6DB33F'}}/>},
+                            {name: 'PostgreSQL', icon: <SiPostgresql fontSize="48px" style={{color: '#336791'}}/>},
 
                         ]}
                         repoLinks={{
@@ -55,24 +55,24 @@ const Projects: React.FC = () => {
                 {/* Add more ProjectCards here */}
                 <Grid item xs={12} md={6}>
                     <ProjectCard
-                        title="New Project"
+                        title="Recipe Vault"
                         description={t('projects.digitalcookbook')}
-                        image="https://via.placeholder.com/1001x339.png?text=Digital+Cookbook+-+Coming+Soon"
+                        image="https://via.placeholder.com/1001x339.png?text=Recipe+Vault+-+Coming+Soon"
                         button_text=''
-                        link="#"
+                        link=''
                         frontend_technologies={[
-                            { name: 'TypeScript', icon: <SiTypescript fontSize="48px" style={{ color: '#3178C6' }} /> },
-                            { name: 'Vue.js', icon: <SiVuedotjs fontSize="48px" style={{ color: '#42b883' }} /> },
-                            { name: 'Nuxt.js', icon: <SiNuxtdotjs fontSize="48px" style={{ color: '#00C58E' }} /> },
-                            { name: 'Bootstrap', icon: <SiBootstrap fontSize="48px" style={{ color: '#7952B3' }} /> },
+                            {name: 'TypeScript', icon: <SiTypescript fontSize="48px" style={{color: '#3178C6'}}/>},
+                            {name: 'Vue.js', icon: <SiVuedotjs fontSize="48px" style={{color: '#42b883'}}/>},
+                            {name: 'Nuxt.js', icon: <SiNuxtdotjs fontSize="48px" style={{color: '#00C58E'}}/>},
+                            {name: 'Bootstrap', icon: <SiBootstrap fontSize="48px" style={{color: '#7952B3'}}/>},
                         ]}
                         backend_technologies={[
-                            { name: 'Golang', icon: <FaGolang fontSize="48px" style={{ color: '#00ADD8' }} /> },
-                            { name: 'PostgreSQL', icon: <SiPostgresql fontSize="48px" style={{ color: '#336791' }} /> },
+                            {name: 'Golang', icon: <FaGolang fontSize="48px" style={{color: '#00ADD8'}}/>},
+                            {name: 'PostgreSQL', icon: <SiPostgresql fontSize="48px" style={{color: '#336791'}}/>},
                         ]}
                         repoLinks={{
-                            frontend: "https://github.com/Barrique-v1",
-                            backend: "https://github.com/Barrique-v1"
+                            frontend: '',
+                            backend: ''
                         }}
                     />
                 </Grid>
