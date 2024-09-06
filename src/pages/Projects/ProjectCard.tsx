62cab58d55a258c8757794aa82d0957e0704a8c3
Barrique-v1 <peters.norman91@gmail.com>
2024-09-06 12:44:49 +0200

    adjust Test and Projects

--- src/pages/Projects/ProjectCard.tsx
+++ src/pages/Projects/ProjectCard.tsx
@@ -26,7 +26,6 @@ type ProjectCardProps = {
     link: string;
     frontend_technologies: Technology[];
     backend_technologies: Technology[];
-    database_technologies: Technology[];
     repoLinks: { frontend: string; backend: string };
 };
 
@@ -44,47 +43,53 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
                                                      link,
                                                      frontend_technologies,
                                                      backend_technologies,
-                                                     database_technologies,
                                                      repoLinks
                                                  }) => (
-    <StyledCard style={{ display: 'flex', flexDirection: 'column' }}>
+    <StyledCard style={{display: 'flex', flexDirection: 'column'}}>
         {/* Project Image */}
-        <CardActionArea component="a" href={link} target="_blank" style={{ flex: '1' }}>
-            <CardMedia component="img" alt={title} image={image} style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+        <CardActionArea component="a" href={link} target="_blank" style={{flex: '1'}}>
+            <CardMedia component="img" alt={title} image={image}
+                       style={{width: '100%', height: '100%', objectFit: 'cover'}}/>
         </CardActionArea>
 
-        <CardContent style={{ display: 'flex', flexDirection: 'column', flex: '3', justifyContent: 'space-between' }}>
+        <CardContent style={{display: 'flex', flexDirection: 'column', flex: '3', justifyContent: 'space-between'}}>
             {/* Project Description */}
-            <div style={{ flex: '1', textAlign: 'center', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+            <div style={{
+                flex: '1',
+                textAlign: 'start',
+                alignItems: 'start',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'start',
+                marginBottom: 20
+            }}>
                 <Typography variant="body1" gutterBottom>
                     {description}
+                    <Button size="small" component="a" href={link} target="_blank">{button_text}</Button>
                 </Typography>
-                <Button size="small" component="a" href={link} target="_blank">{button_text}</Button>
             </div>
 
             {/* Technologie Titles */}
-            <Grid container spacing={2} justifyContent="center" alignItems="center" textAlign="center" style={{ flex: '1' }}>
-                <Grid item xs={4}>
+            <Grid container spacing={0} justifyContent="center" alignItems="end" textAlign="center"
+                  style={{flex: '1'}}>
+                <Grid item xs={6}>
                     <Typography variant="overline">Frontend</Typography>
                     <IconButton href={repoLinks.frontend} target="_blank" aria-label="GitHub Frontend Repo">
-                        <GitHubIcon style={{ color: '#333', fontSize: 'large' }} />
+                        <GitHubIcon style={{color: '#333', fontSize: 'large'}}/>
                     </IconButton>
                 </Grid>
-                <Grid item xs={4}>
+                <Grid item xs={6}>
                     <Typography variant="overline">Backend</Typography>
                     <IconButton href={repoLinks.backend} target="_blank" aria-label="GitHub Backend Repo">
-                        <GitHubIcon style={{ color: '#333', fontSize: 'large' }} />
+                        <GitHubIcon style={{color: '#333', fontSize: 'large'}}/>
                     </IconButton>
-                </Grid>
-                <Grid item xs={4}>
-                    <Typography variant="overline">Data</Typography>
                 </Grid>
             </Grid>
 
             {/* Technologie Icons */}
-            <Grid container spacing={2} justifyContent="center" alignItems="center" style={{ flex: '1' }}>
-                <Grid item xs={4}>
-                    <Grid container spacing={2} justifyContent="center" alignItems="center">
+            <Grid container spacing={0} justifyContent="center" alignItems="center" style={{flex: '1'}}>
+                <Grid item xs={6}>
+                    <Grid container spacing={0} justifyContent="center" alignItems="center">
                         {frontend_technologies.map((tech, index) => (
                             <Grid item key={index}>
                                 <Tooltip title={tech.name}>
@@ -96,22 +101,9 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
                         ))}
                     </Grid>
                 </Grid>
-                <Grid item xs={4}>
-                    <Grid container spacing={2} justifyContent="center" alignItems="center">
+                <Grid item xs={6}>
+                    <Grid container spacing={0} justifyContent="center" alignItems="center">
                         {backend_technologies.map((tech, index) => (
-                            <Grid item key={index}>
-                                <Tooltip title={tech.name}>
-                                    <IconButton aria-label={tech.name}>
-                                        {tech.icon}
-                                    </IconButton>
-                                </Tooltip>
-                            </Grid>
-                        ))}
-                    </Grid>
-                </Grid>
-                <Grid item xs={4}>
-                    <Grid container spacing={2} justifyContent="center" alignItems="center">
-                        {database_technologies.map((tech, index) => (
                             <Grid item key={index}>
                                 <Tooltip title={tech.name}>
                                     <IconButton aria-label={tech.name}>
--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { Grid, Typography, Box } from '@mui/material';
 import ProjectCard from './ProjectCard';
 import BucksBuddyLogo from '../../assets/images/BucksBuddy_Logo.png';
-import { SiTypescript, SiVuedotjs, SiSpringboot, SiPostgresql } from 'react-icons/si';
-import { FaGolang, FaVuejs } from "react-icons/fa6";
+import { SiTypescript, SiVuedotjs, SiSpringboot, SiPostgresql, SiNuxtdotjs, SiBootstrap } from 'react-icons/si';
+import { FaGolang } from "react-icons/fa6";
 import { ReactComponent as JavaIcon } from '../../assets/icons/java.svg';
 import { useTranslation } from "react-i18next";
 
@@ -38,13 +38,13 @@ const Projects: React.FC = () => {
                         frontend_technologies={[
                             { name: 'TypeScript', icon: <SiTypescript fontSize="48px" style={{ color: '#3178C6' }} /> },
                             { name: 'Vue.js', icon: <SiVuedotjs fontSize="48px" style={{ color: '#42b883' }} /> },
+                            { name: 'Bootstrap', icon: <SiBootstrap fontSize="48px" style={{ color: '#7952B3' }} /> },
                         ]}
                         backend_technologies={[
                             { name: 'Java', icon: <JavaIcon width={48} height={48} /> },
                             { name: 'Spring Boot', icon: <SiSpringboot fontSize="48px" style={{ color: '#6DB33F' }} /> },
-                        ]}
-                        database_technologies={[
                             { name: 'PostgreSQL', icon: <SiPostgresql fontSize="48px" style={{ color: '#336791' }} /> },
+
                         ]}
                         repoLinks={{
                             frontend: "https://github.com/Barrique-v1/Travel_Fund_Frontend",
@@ -58,16 +58,16 @@ const Projects: React.FC = () => {
                         title="New Project"
                         description={t('projects.digitalcookbook')}
                         image="https://via.placeholder.com/1001x339.png?text=Digital+Cookbook+-+Coming+Soon"
-                        button_text={t('projects.button')}
+                        button_text=''
                         link="#"
                         frontend_technologies={[
                             { name: 'TypeScript', icon: <SiTypescript fontSize="48px" style={{ color: '#3178C6' }} /> },
                             { name: 'Vue.js', icon: <SiVuedotjs fontSize="48px" style={{ color: '#42b883' }} /> },
+                            { name: 'Nuxt.js', icon: <SiNuxtdotjs fontSize="48px" style={{ color: '#00C58E' }} /> },
+                            { name: 'Bootstrap', icon: <SiBootstrap fontSize="48px" style={{ color: '#7952B3' }} /> },
                         ]}
                         backend_technologies={[
                             { name: 'Golang', icon: <FaGolang fontSize="48px" style={{ color: '#00ADD8' }} /> },
-                        ]}
-                        database_technologies={[
                             { name: 'PostgreSQL', icon: <SiPostgresql fontSize="48px" style={{ color: '#336791' }} /> },
                         ]}
                         repoLinks={{
